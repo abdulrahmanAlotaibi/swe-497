8e9feb47776fb3f8bd0e5d7cf8b9bfa8bbbd65a4
eldh97 <eldh97@outlook.com>
2020-11-10 11:22:05 +0300

    u

--- ethra-client/src/components/nav/Nav.js
+++ ethra-client/src/components/nav/Nav.js
@@ -4,26 +4,25 @@ import { Link } from "react-router-dom";
 // import axios from "axios";
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';import "./Nav.scss";
 import Logo from "components/logo"
-
+import SearchIcon from '@material-ui/icons/Search';
 function Nav() {
   return (
     <nav className="nav">
       <ul className="nav__start">
         <Logo/>
-        <ul className="explore-list"><span>Explore</span>
-        <div><KeyboardArrowDownIcon className="explore-list__icon"/></div>
-        </ul>
+        <div>Courses</div>
+                <div>Books</div>
       </ul>
 
-      <ul className="nav__middle">
+      {/* <ul className="nav__middle">
         <Search />
-      </ul>
+      </ul> */}
 
       <ul className="nav__end">
-        <li className="nav__item">
-          <Link to="/search">My Courses</Link>
-        </li>
-
+      <li>
+        <SearchIcon className="nav__item nav__item--search"/>
+      </li>
+    
         <li className="nav__item">
           <Link className="signin-link" to="/signin">
             Sign In
